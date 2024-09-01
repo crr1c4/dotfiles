2da463178bef7010950121ecf8cee9aa1cd872e8
Christian Venegas <venegaschristian014@gmail.com>
2024-09-01 15:17:20 -0600

    fixed move commands

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,11 +3,6 @@ local keymap = vim.keymap.set
 
 keymap('', '<Space>', '<Nop>', opts)
 
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
-
 keymap('n', '<C-Up>', ':resize +2<CR>', opts)
 keymap('n', '<C-Down>', ':resize -2<CR>', opts)
 keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
