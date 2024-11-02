0aed3b78ca1c980ef928f12250a4fe75281efc6a
Christian Venegas <venegaschristian014@gmail.com>
2024-11-02 12:45:47 -0600

    cmp fixed

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require("core.settings")
 require("core.plugins")
+require("core.lsp")
 require("core.keymaps")
 
 
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,15 +3,7 @@ return {
   event = { 'LspAttach' },
   version = 'v0.*',
   opts = {
-    keymap = {
-      show = '<C-space>',
-      hide = { '<Esc>' },
-      accept = '<CR>',
-      select_prev = { '<Up>' },
-      select_next = { '<Down>'},
-      scroll_documentation_up = '<C-b>',
-      scroll_documentation_down = '<C-n>',
-    },
+    keymap = "default", -- TODO: Change accept to <CR>
     nerd_font_variant = 'normal',
   }
 }
